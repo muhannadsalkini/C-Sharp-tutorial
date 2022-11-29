93101bd3c55b4d1b73e83cebc46fcfc8327c9518
muhannadsalkini <mohanad.salkini@gmail.com>
2022-11-29 22:24:53 +0300

    Added week7 1. homework

---
+++ Week7_homework1/Week7_homework1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7_homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(1910205563, "Muhannad", "Salkini", 100.00, 100.00, 100.00, "Karabuk Universitesi");
+
+            int var =0;
+
+            Console.WriteLine("1- Ogrenci Bilgileri Goster\n" +
+                    "2- Ogrenci Ortalamasi Goster\n" +
+                    "3- Ogrencinin Okuluni Ogren\n" +
+                    "4- Cikis Yap\n");
+
+            while (var != 4)
+            {
+                Console.WriteLine("Giris Yapin: ");
+                var = Convert.ToInt32(Console.ReadLine());
+                switch (var)
+                {
+                    case 1:
+                        Console.WriteLine("Ad - Soyad: {0}\n", s1.ogrenciBilgileriGoster());
+                        break;
+                    case 2:
+                        Console.WriteLine("Ortalama: {0}\n", s1.ogrenciOrtalamasiBul());
+                        break;
+                    case 3:
+                        Console.WriteLine("Okul: {0}\n", s1.okulGetir());
+                        break;
+                    case 4:
+                        Console.WriteLine("Cikis yapildi!\n");
+                        break;
+                    default:
+                        Console.WriteLine("Hatali Giris Yapildi!\n");
+                        break;
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week7_homework1/Week7_homework1/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7_homework1
+{
+    internal class Student
+    {
+        private int ogrenciNo;
+        private string isim;
+        private string soysim;
+        private double vize1;
+        private double vize2;
+        private double final;
+        private string okulIsmi;
+
+        public Student(int ogrenciNo, string isim, string soysim, double vize1, double vize2, double final, string okulIsmi)
+        {
+            this.ogrenciNo = ogrenciNo;
+            this.isim = isim;
+            this.soysim = soysim;
+            this.vize1 = vize1;
+            this.vize2 = vize2;
+            this.final = final;
+            this.okulIsmi = okulIsmi;
+        }
+
+        public double ogrenciOrtalamasiBul()
+        {
+            return vize1 * 0.2 + vize2 * 0.2 + final * 0.6;
+        }
+
+        public string okulGetir()
+        {
+            return okulIsmi;
+        }
+
+        public string ogrenciBilgileriGoster()
+        {
+            return isim +" "+ soysim;
+        }
+
+
+    }
+}
