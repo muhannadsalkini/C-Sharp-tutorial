3c664cfae680f87dc7f78ec01aba4879fa2035aa
muhannadsalkini <mohanad.salkini@gmail.com>
2022-11-29 22:26:42 +0300

    Added week7 2. homework

---
+++ Week7_homework2/Week7_homework2/Personel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Week7_homework2
+{
+    internal class Personel
+    {
+        private string name;
+        private string surname;
+        private string TC;
+
+        public Personel(string name, string surname, string TC)
+        {
+            this.name = name;
+            this.surname = surname;
+            tc = TC;
+        }
+
+        public string tc
+        {
+            get {
+                if (TC != null)
+                    return TC.Substring(0, 5)+"******";
+                else
+                    return "No TC number!";
+            }
+            set {
+                if (value.Length == 11 && Regex.IsMatch(value, @"^\d+$")) // if TC is number and 11 charecter
+                    TC = value;
+                else
+                    Console.WriteLine("Mising or worng data for TC!\n");
+            }
+        }
+    }
+}
---
+++ Week7_homework2/Week7_homework2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7_homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel p1 = new Personel("Muhannad", "Salkini", "12345678901");
+
+
+            Console.WriteLine("TC: {0}",p1.tc);
+            Console.ReadKey();
+        }
+    }
+}
